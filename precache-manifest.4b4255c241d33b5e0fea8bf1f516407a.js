fe7327a9fa62afb97ce3d47af24c7113e1f3c4fb
Jenny Rose Gutierrez <jennygutierrez@Jennys-MacBook-Air.local>
2019-01-12 11:54:31 -0800

    Updates

---
+++ precache-manifest.4b4255c241d33b5e0fea8bf1f516407a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed8f09ba6d1527b95f71",
+    "url": "/portfolio/static/css/main.25e3c6ff.chunk.css"
+  },
+  {
+    "revision": "ed8f09ba6d1527b95f71",
+    "url": "/portfolio/static/js/main.ed8f09ba.chunk.js"
+  },
+  {
+    "revision": "c47e67fe727a3909d5a7",
+    "url": "/portfolio/static/js/1.c47e67fe.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "472d27ef7fcf0069919dbf8fb355a367",
+    "url": "/portfolio/static/media/boomtown.472d27ef.png"
+  },
+  {
+    "revision": "361f80dbf62041c1feef4a961c680faf",
+    "url": "/portfolio/static/media/mvmnt2.361f80db.png"
+  },
+  {
+    "revision": "e669d59f212040ca358f217f7195bd61",
+    "url": "/portfolio/static/media/mvmnt1.e669d59f.png"
+  },
+  {
+    "revision": "1a7aaee63d40d05693b072a07f3b075d",
+    "url": "/portfolio/static/media/r10.1a7aaee6.png"
+  },
+  {
+    "revision": "ce1bffb6dc921cca2121f81e58323ecf",
+    "url": "/portfolio/static/media/r101.ce1bffb6.png"
+  },
+  {
+    "revision": "bbd6de26a88897681967e570bf7478dc",
+    "url": "/portfolio/static/media/todo.bbd6de26.png"
+  },
+  {
+    "revision": "f3a81a14bc88c94fcad5bdb0a3ce4522",
+    "url": "/portfolio/static/media/carpool.f3a81a14.png"
+  },
+  {
+    "revision": "bf178786b54e490b17b07d4cc850ffc3",
+    "url": "/portfolio/static/media/jenny.bf178786.png"
+  },
+  {
+    "revision": "4249b136f03f255d414546441a6bae65",
+    "url": "/portfolio/static/media/IMG_7594.4249b136.JPG"
+  },
+  {
+    "revision": "a61f60d664c77626c7f7cb8eb9698d5c",
+    "url": "/portfolio/index.html"
+  }
+];
